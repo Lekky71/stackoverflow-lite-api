f421d5320f9766a9a6863c7e3dbcc401ffde3b90
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-06 10:15:51 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import indexRouter from './routes/index';
 import authController from './controllers/auth.controller';
 import questionController from './controllers/question.controller';
@@ -23,6 +24,7 @@ const apiVersion = 'api/v1';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
