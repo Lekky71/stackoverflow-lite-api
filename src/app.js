d4842eda148c4bf593cd3e78fce32a77f534c67f
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 09:11:15 +0100

    removed cors

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
