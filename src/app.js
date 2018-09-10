1e7ff024a2dacbc42e379680d9f80ef2d61a69e0
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 09:27:29 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -25,14 +25,8 @@ const apiVersion = 'api/v1';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(cors());
-// app.all('', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   // Auth Each API Request created by user.
-//   next();
-// });
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
