ad1e52a105b9816b1caff1c4b3bc51f4f05cf2fd
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 10:19:38 +0100

    added allowed headers

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ if (!isDevEnv) {
       origin: 'https://stack-overflow-lite-frontend.herokuapp.com',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     }));
+    app.use(cors());
     app.options('*', cors()); // include before other routes
     next();
   });
