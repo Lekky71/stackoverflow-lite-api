742d7c8df37fe9fc33724fae11f7d2377c37981f
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 08:59:50 +0100

    add links in root page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to StackOverFlowLite API', pivotal_tracker: 'https://...  ', swagger: 'https://...' });
+  // res.json({
+  //   message: 'Welcome to StackOverFlowLite API',
+  //   pivotal_tracker: 'https://www.pivotaltracker.com/projects/2192115',
+  //   swagger: 'https://app.swaggerhub.com/apis/Oluwalekae/StackOFlow-Lite/1.0',
+  // });
+  return res.send(`<html>
+                       <p>Welcome to StackOverFlowLite API</p>
+                       <a href="https://www.pivotaltracker.com/projects/2192115">pivotal_tracker_link</a>
+                       <br/>
+                       <a href="https://app.swaggerhub.com/apis/Oluwalekae/StackOFlow-Lite/1.0">swagger_link</a>
+                    </html>`);
 });
 
 module.exports = router;
