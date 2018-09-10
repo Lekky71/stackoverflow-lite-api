b27a2f7c7d56abf444442b72f64356fd9047e6bf
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 11:27:17 +0100

    added frontend files to a separate folder due to cors problem

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,13 @@ import express from 'express';
 const router = express.Router();
 const viewFolder = `${__dirname.replace('dist/routes', 'public')}/src/`; // view folder
 /* GET home page. */
+router.get('/api/v1', (req, res) => res.send(`<html>
+                       <p>Welcome to StackOverFlowLite API</p>
+                       <a href="https://www.pivotaltracker.com/projects/2192115" target="_blank">pivotal_tracker_link</a>
+                       <br/>
+                       <a href="https://app.swaggerhub.com/apis/Oluwalekae/StackOFlow-Lite/1.0" target="_blank">swagger_link</a>
+                    </html>`));
 router.get('/', (req, res) => {
-  // res.json({
-  //   message: 'Welcome to StackOverFlowLite API',
-  //   pivotal_tracker: 'https://www.pivotaltracker.com/projects/2192115',
-  //   swagger: 'https://app.swaggerhub.com/apis/Oluwalekae/StackOFlow-Lite/1.0',
-  // });
-  // return res.send(`<html>
-  //                      <p>Welcome to StackOverFlowLite API</p>
-  //                      <a href="https://www.pivotaltracker.com/projects/2192115">pivotal_tracker_link</a>
-  //                      <br/>
-  //                      <a href="https://app.swaggerhub.com/apis/Oluwalekae/StackOFlow-Lite/1.0">swagger_link</a>
-  //                   </html>`);
   res.sendFile(`${viewFolder}index.html`);
 });
 
