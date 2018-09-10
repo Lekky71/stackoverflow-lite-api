fea8199fcbb42ac39771c0621949ad1807dc309a
Oluwaleke Fakorede <hashilekky@gmail.com>
2018-09-10 10:04:41 +0100

    added allowed headers

--- src/app.js
+++ src/app.js
@@ -30,6 +30,11 @@ if (!isDevEnv) {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://stack-overflow-lite-frontend.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, x-access-token, Accept');
+    app.use(cors({
+      origin: 'https://stack-overflow-lite-frontend.herokuapp.com',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }));
+    app.options('*', cors()); // include before other routes
     next();
   });
 } else {
